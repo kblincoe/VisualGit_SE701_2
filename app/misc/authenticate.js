9be96826b2c9b6f731025a82f2a05883422a31e9
enginefong <enginefong@gmail.com>
2018-03-08 00:32:55 +1300

    AES-256 CBC encryption of username, password, last login time when remember me is checked, decryption and machine id check on app start up
    Added auto fill on app open
    
    add checkbox, encrypt to json, start up function created
    
    recompile js files and fix color issues with remember me label

--- app/misc/authenticate.js
+++ app/misc/authenticate.js
@@ -1,4 +1,7 @@
 var github = require("octonode");
+var rememberMe;
+var machineId = require("node-machine-id");
+var fs = require('fs');
 var aid, atoken;
 var client;
 var avaterImg;
@@ -14,7 +17,61 @@ function signInHead(callback) {
 function signInPage(callback) {
     setCredentials(document.getElementById("username").value, document.getElementById("password").value);
     console.log('user has logged in successfully');
+    rememberMe = document.getElementById("rememberme").checked;
     getUserInfo(callback);
+}
+function autoFillPassword(callback) {
+    fs.stat("login.json", function (err, stats) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            var obj = JSON.parse(fs.readFileSync('login.json', 'utf8'));
+            var crypto_1 = require("crypto");
+            var d1 = crypto_1.createDecipher('aes-256-cbc', 'secretpassword');
+            var saltedPassword = d1.update(obj.loginInfo[0]["password"], 'hex', 'utf8');
+            saltedPassword = saltedPassword + d1.final('utf8');
+            var pw = saltedPassword.substring(0, saltedPassword.length - obj.loginInfo[0]["split"]);
+            var d2 = crypto_1.createDecipher('aes-256-cbc', 'secrettime');
+            var saltedTime = d2.update(obj.loginInfo[0]["lastLogin"], 'hex', 'utf8');
+            saltedTime = saltedTime + d2.final('utf8');
+            var lastLogin = saltedTime.substring(0, saltedTime.length - obj.loginInfo[0]["split"]);
+            var machineId_1 = require("node-machine-id");
+            var uidSalt = saltedPassword.substring(pw.length, saltedPassword.length);
+            var uidTime = saltedTime.substring(lastLogin.length, saltedTime.length);
+            //login details are deleted it does not belong to the original computer the details were generated for or if login details is older than 7 days
+            if ((machineId_1.machineIdSync() == uidSalt) && (uidSalt == uidTime) && ((new Date).getTime() - lastLogin < 604800)) {
+                document.getElementById("username").value = obj.loginInfo[0]["username"];
+                document.getElementById("password").value = pw;
+                document.getElementById("rememberme").checked = true;
+            }
+            else {
+                deleteLoginDetails();
+            }
+        }
+    });
+}
+function deleteLoginDetails() {
+    fs.stat("login.json", function (err, stats) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            fs.unlinkSync("login.json");
+        }
+    });
+}
+function saveLoginDetails(username, password) {
+    var uid = machineId.machineIdSync();
+    var crypto = require("crypto");
+    var c1 = crypto.createCipher('aes-256-cbc', 'secretpassword');
+    var pw = c1.update(password + uid, 'utf8', 'hex') + c1.final('hex');
+    var c2 = crypto.createCipher('aes-256-cbc', 'secrettime');
+    var time = c2.update((new Date).getTime() + uid, 'utf8', 'hex') + c2.final('hex');
+    var obj = { loginInfo: [] };
+    obj.loginInfo.push({ username: username, password: pw, lastLogin: time, split: uid.length });
+    var json = JSON.stringify(obj);
+    fs.writeFile('login.json', json, 'utf8');
 }
 function setCredentials(username, password) {
     cred = Git.Cred.userpassPlaintextNew(username, password);
@@ -30,6 +87,9 @@ function getUserInfo(callback) {
             displayModal(err);
         }
         else {
+            if (rememberMe) {
+                saveLoginDetails(document.getElementById("username").value, document.getElementById("password").value);
+            }
             avaterImg = Object.values(data)[2];
             // let doc = document.getElementById("avater");
             // doc.innerHTML = "";
