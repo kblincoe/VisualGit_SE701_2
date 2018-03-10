1676128d9253d1417652a95ec3911190222e54e6
CJ <bchj513@hotmail.com>
2018-03-10 18:20:49 +1300

    Add more colour themes (#54)
    * Removed greek question mark
    
    * added two more elements to the drop down list
    
    * Made changes to color.ts and authenticate.component.ts
    
    * Added styling classes to styles.css file
    
    * Compiled authenticate.component.js after resolving merge conflict

--- app/components/authenticate.component.ts
+++ app/components/authenticate.component.ts
@@ -13,6 +13,8 @@ import { openForgotPasswordPage } from "../misc/forgotPassword";
           </button>
           <ul class="dropdown-menu" id="color-dropdown" role="menu" aria-labelledby="branch-name">
             <li class="white" onclick="changeColor('white')">white</li>
+            <li class="green" onclick="changeColor('green')">green</li>
+            <li class="blue" onclick="changeColor('blue')">blue</li>
             <li class="default" onclick="changeColor('default')">default</li>
           </ul>
         </div>
--- app/misc/git.ts
+++ app/misc/git.ts
@@ -578,7 +578,7 @@ function displayModifiedFiles() {
             if (fileElement.className === "file file-created") {
               printNewFile(file.filePath);
             } else {
-              printFileDiff(file.filePath)；
+              printFileDiff(file.filePath);
             }
           } else {
             hideDiffPanel();
