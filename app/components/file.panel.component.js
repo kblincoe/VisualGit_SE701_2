302f823b5dba3a6e9480a77b8633ad8636dc8659
softeng-701 <36385673+softeng-701@users.noreply.github.com>
2018-03-13 11:57:09 +1300

    Revert "add git clean functionality"

--- app/components/file.panel.component.js
+++ app/components/file.panel.component.js
@@ -5,17 +5,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
 var FilePanelComponent = (function () {
     function FilePanelComponent() {
     }
-    FilePanelComponent = __decorate([
-        core_1.Component({
-            selector: "file-panel",
-            template: "\n  <div class=\"file-panel\" id=\"file-panel\">\n\n    <div class=\"modified-files-header\" id=\"modified-files-header\">\n      <p class=\"select-all-message\" id=\"select-all-message\">Select all</p>\n      <input onClick=\"setAllCheckboxes(this);\" type=\"checkbox\" class=\"select-all-checkbox\" id=\"select-all-checkbox\"/>\n    </div>\n\n    <div class=\"files-changed\" id=\"files-changed\">\n      <p class=\"modified-files-message\" id=\"modified-files-message\">Your modified files will appear here</p>\n      <div class=\"file\" *ngFor=\"let file of files\">\n        <p>{{file}}</p>\n      </div>\n    </div>\n\n    <div class=\"commit-panel\" id=\"commit-panel\">\n      <textarea placeholder=\"Describe your changes here...\" class=\"commit-message-input\" id=\"commit-message-input\"></textarea>\n      <button class=\"commit-button\" id=\"commit-button\">Commit</button>\n    </div>\n\n    <div class=\"clean-panel\" id=\"clean-panel\">\n      <button class=\"clean-button\" id=\"clean-button\">Clean</button>\n    </div>\n\n  </div>\n  "
-        })
-    ], FilePanelComponent);
     return FilePanelComponent;
 }());
+FilePanelComponent = __decorate([
+    core_1.Component({
+        selector: "file-panel",
+        template: "\n  <div class=\"file-panel\" id=\"file-panel\">\n\n    <div class=\"modified-files-header\" id=\"modified-files-header\">\n      <p class=\"select-all-message\" id=\"select-all-message\">Select all</p>\n      <input onClick=\"setAllCheckboxes(this);\" type=\"checkbox\" class=\"select-all-checkbox\" id=\"select-all-checkbox\"/>\n    </div>\n\n    <div class=\"files-changed\" id=\"files-changed\">\n      <p class=\"modified-files-message\" id=\"modified-files-message\">Your modified files will appear here</p>\n      <div class=\"file\" *ngFor=\"let file of files\">\n        <p>{{file}}</p>\n      </div>\n    </div>\n\n    <div class=\"commit-panel\" id=\"commit-panel\">\n      <textarea placeholder=\"Describe your changes here...\" class=\"commit-message-input\" id=\"commit-message-input\"></textarea>\n      <button class=\"commit-button\" id=\"commit-button\">Commit</button>\n    </div>\n\n  </div>\n  "
+    }),
+    __metadata("design:paramtypes", [])
+], FilePanelComponent);
 exports.FilePanelComponent = FilePanelComponent;
